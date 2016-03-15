945ab91ebef73568eee40d3e4b775c84ced2ade3
Jose Torres <josse_1397@hotmail.com>
2016-03-15 11:32:25 -0400

    Actualizando app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@
 var HelloWorldLayer = cc.Layer.extend({
     sprFondo:null,
     sprConejo:null,
+    bombas: [],
+    creaBomba: function(){
+		
+		var bomba = new cc.Sprite(res.bomba_png);
+		bomba.setScale(0.4,0.4);
+        bomba.setPosition(this.random(1,480), this.size.height );
+        this.addChild(bomba, 1);
+		var moveto = cc.moveTo(this.random(1,9), this.sprConejo.getPositionX(), this.sprConejo.getPositionY());
+		bomba.runAction(moveto);
+		this.bombas.push(bomba);		
+		
+	},
     ctor:function () {
         this._super();
         //Obteniendo el tamaño de la pantalla
@@ -11,7 +23,7 @@ var HelloWorldLayer = cc.Layer.extend({
         this.sprFondo = new cc.Sprite(res.fondo_png);
         this.sprFondo.setPosition(size.width / 2,size.height / 2);
         this.addChild(this.sprFondo, 0);
-        
+        this.schedule(this.creaBomba,3);
         //posicionando la imagen de fondo
         this.sprConejo = new cc.Sprite(res.conejo_png);
         this.sprConejo.setPosition(size.width / 2,size.height * 0.15);
